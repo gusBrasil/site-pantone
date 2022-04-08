a4f3f9fae5ad5741c09fb9e2c168fe04b207c41a
gusBrasil <gustavomedbra@gmail.com>
2022-04-07 22:50:52 -0300

    update

--- scroll.js
+++ scroll.js
@@ -1,19 +1,19 @@
-    // Scroll to specific values
-    // scrollTo is the same
-    window.scroll({
-        top: 2500,
+let myButton = document.getElementById('button-top');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+    window.scrollTo({
+        top: 0,
         left: 0,
         behavior: 'smooth'
-    });
-
-    // Scroll certain amounts from current position 
-    window.scrollBy({
-        top: 100, // could be negative value
-        left: 0,
-        behavior: 'smooth'
-    });
-
-    // Scroll to a certain element
-    document.querySelector('.hello').scrollIntoView({
-        behavior: 'smooth'
-    });
+    })
+}
